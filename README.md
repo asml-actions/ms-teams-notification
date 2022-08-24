e672bae1228e23ca564599ed65bba0d80b939a5b
Rob Bos <rob.bos@asml.com>
2022-08-24 20:37:11 +0200

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ms-teams-notification
 Internal action to send a notification to MS Teams
+
+# Inputs
+
+|Input|Description|Required|Default value|
+|---|---|---|---|
+|title|Title for the notification|Yes|None|
+|text|Text for the notification|Yes|None|
+|button_text|Text to display in the action button|Yes|None|
+|url|Url for the action button|No|A link back to the caller workflow run|
+|teams_webhook_url|Teams webhook url|Yes|None|
+# Usage
+
+``` yaml
+uses: asml-gh/ms-teams-notification
+with: 
+  title: 'Title for the notification'
+  text: 'Text for the notification'
+  button_text: 'Text to display in the action button'
+  url: "https://asml.com"
+```
---
+++ send-notification.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# load input parameters
+title="Message from runner check workflow"
+if [ -n "$1" ] 
+then
+  echo " loading title from args"
+  title=$1 
+fi
+
+text="This is my test text"
+if [ -n "$2" ]
+then
+  echo " loading text from args"
+  text=$2
+fi
+
+button="Go to workflow"
+if [ -n "$3" ]
+then
+  echo " loading button from args"
+  button=$3
+fi
+
+button_link="$GITHUB_SERVER_URL"
+if [ -n "$4" ]
+then
+  echo " loading button link from args"
+  button_link=$4
+fi
+
+# load url from environment setting
+echo "Found this Teams url to work with [$TEAMS_WEBHOOK]"
+echo " - title: [$title]"
+echo " - message: [$text]"
+echo " - button: [$button]"
+echo " - button_link: [$button_link]"
+
+# remove first and last char
+text=${text//'['/''}
+text=${text//']'/''}
+# remove double quotes
+text=${text//'\"'/''}
+text=${text//'"'/''}
+
+
+if [ ${#text} == 0 ]
+then
+  echo "Nothing to report"
+  exit 0
+fi
+# add the link as a new line
+array=$array', Link: '$button_link
+
+# replace comma's with newlines
+text=${text//','/'\n\r'}
+array=$text
+
+
+# real array: 
+#array=$(echo $text | tr "," "\n\r")
+
+# docs for this webhook are here: https://docs.microsoft.com/en-us/outlook/actionable-messages/send-via-connectors
+# prepare the body
+body='{
+  "@context": "https://schema.org/extensions",
+  "@type": "MessageCard",
+  "themeColor": "0072C6",
+  "title": "msg_title",
+  "text": "msg_text",
+  "potentialAction": [
+    {
+      "@type": "OpenUri",
+      "name": "msg_button",
+      "targets": [
+        { "os": "default", "uri": "msg_buttonlink" }
+      ]
+    }
+  ]
+}' 
+
+body=${body/msg_title/$title}
+body=${body/msg_text/$array}
+body=${body/msg_button/$button}
+body=${body/msg_buttonlink/$button_link}
+
+if [ -z "$body" ]
+then
+  echo "No body found!"
+  exit
+fi
+
+echo "body = "
+echo $body
+
+curl -i \
+  -d "$body" \
+  -H "Accept: application/json" \
+  $TEAMS_WEBHOOK
+
+if [ "$?" -ne 0 ]
+then 
+  echo "Error sending webhook"
+  exit 1
+fi
+echo ""
+echo "Done"
