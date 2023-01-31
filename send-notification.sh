5d59104e9ccf5701424bd1bf2d1adc81171ad788
Vineeth <vineeth.puthiya.veettil@asml.com>
2023-01-31 09:16:19 +0100

    Test mention in teams channel

--- send-notification.sh
+++ send-notification.sh
@@ -29,12 +29,37 @@ then
   button_link=$4
 fi
 
+spoc_name=""
+if [ -n "$5" ]
+then
+  echo " loading spoc name from args"
+  spoc_name=$5
+fi
+
+backup_spoc_name=""
+if [ -n "$6" ]
+then
+  echo " loading backup spoc name from args"
+  backup_spoc_name=$6
+fi
+
+spoc_email=""
+if [ -n "$7" ]
+then
+  echo " loading spoc email from args"
+  spoc_email=$7
+fi
+
+backup_spoc_email=""
+if [ -n "$8" ]
+then
+  echo " loading backup spoc email from args"
+  button_link=$8
+fi
+
 # load url from environment setting
-echo "Found this Teams url to work with [$TEAMS_WEBHOOK]"
-echo " - title: [$title]"
-echo " - message: [$text]"
-echo " - button: [$button]"
-echo " - button_link: [$button_link]"
+
+
 
 # remove first and last char
 text=${text//'['/''}
@@ -63,23 +88,55 @@ array=$text
 # docs for this webhook are here: https://docs.microsoft.com/en-us/outlook/actionable-messages/send-via-connectors
 # prepare the body
 body='{
-  "@context": "https://schema.org/extensions",
-  "@type": "MessageCard",
-  "themeColor": "0072C6",
-  "title": "msg_title",
-  "text": "msg_text",
-  "potentialAction": [
-    {
-      "@type": "OpenUri",
-      "name": "msg_button",
-      "targets": [
-        { "os": "default", "uri": "msg_buttonlink" }
-      ]
-    }
-  ]
+    "type": "message",
+    "attachments": [
+        {
+        "contentType": "application/vnd.microsoft.card.adaptive",
+        "content": {
+            "type": "AdaptiveCard",
+            "body": [
+                {
+                    "type": "TextBlock",
+                    "size": "Medium",
+                    "weight": "Bolder",
+                    "text": "msg_title"
+                },
+                {
+                    "type": "TextBlock",
+                    "text": "msg_text"
+                }
+            ],
+            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+            "version": "1.0",
+            "msteams": {
+                "entities": [
+                    {
+                        "type": "mention",
+                        "text": "<at>spoc_name</at>",
+                        "mentioned": {
+                          "id": "spoc_email",
+                          "name": "spoc_name"
+                        }
+                      },
+                      {
+                        "type": "mention",
+                        "text": "<at>backup_spoc_name</at>",
+                        "mentioned": {
+                          "id": "backup_spoc_email",
+                          "name": "backup_spoc_name"
+                        }
+                      }
+                ]
+            }
+        }
+    }]
 }' 
 
 body=${body/msg_title/$title}
+body=${body/spoc_name/$spoc_name}
+body=${body/backup_spoc_name/$backup_spoc_name}
+body=${body/spoc_email/$spoc_email}
+body=${body/backup_spoc_email/$backup_spoc_email}
 body=${body/msg_text/$array}
 body=${body/msg_button/$button}
 body=${body/msg_buttonlink/$button_link}
@@ -96,8 +153,7 @@ echo $body
 curl -i \
   -d "$body" \
   -H "Accept: application/json" \
-  -H 'Content-Type: application/json' \
-  $TEAMS_WEBHOOK
+  $TEAMS_WEBHOOK 
 
 if [ "$?" -ne 0 ]
 then 
